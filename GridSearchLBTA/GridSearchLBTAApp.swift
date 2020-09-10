c1994cea1b8fea18e9002801e64534dbafd269aa
ibalborde <maximiliano_009@hotmail.com>
2020-09-10 10:29:47 -0300

    Initial Commit

---
+++ GridSearchLBTA/GridSearchLBTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchLBTAApp.swift
+//  GridSearchLBTA
+//
+//  Created by Maximiliano Ibalborde on 10/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchLBTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
